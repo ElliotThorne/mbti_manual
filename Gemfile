931ea7041376eb05cc414332b396a8ef505ae012
ElliotThorne <elliotemporium@hotmail.co.uk>
2015-05-03 00:03:05 +0100

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -12,13 +12,14 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 
 group :development, :test do
-  
+ 
   gem 'byebug'
-
- 
   gem 'web-console', '~> 2.0'
-
- 
   gem 'spring'
 end
 
+group :test do
+  gem 'minitest-reporters', '1.0.5'
+  gem 'mini_backtrace',     '0.1.3'
+  gem 'guard-minitest',     '2.3.1'
+end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,24 +3,24 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   
   def setup
-  @title = " | The MBTI Manual"
+  @title = "MBTI Manual"
 end
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home" "#{@title}" 
+    assert_select "title", "#{@title}"
   end
 
   test "should get info" do
     get :info
     assert_response :success
-    assert_select "title", "Info" "#{@title}" 
+    assert_select "title", "Info | " "#{@title}" 
   end
 
   test "should get types" do 
   	get :types 
   	assert_response :success
-  	assert_select "title", "16 Types" "#{@title}" 
+  	assert_select "title", "16 Types | " "#{@title}" 
   end
 end
